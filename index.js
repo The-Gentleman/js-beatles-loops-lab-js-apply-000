570cf596a9b34861dc3bd823e0e785814aa8c0f2
Zeshan Raja <zraja4402@gmail.com>
2019-10-14 20:36:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+var resultArray = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    var musicianStr = musicians[i];
+    var instrumentStr = instruments[i];
+    resultArray.push (musicianStr + ' plays ' + instrumentStr);
+  }
+  
+ return resultArray;  
+}
+theBeatlesPlay('John Lennon', 'guitar');
+
+// + Create a function `johnLennonFacts`. This function will accept one argument, an
+// array of facts about John Lennon (note that it might not be exactly the
+// following facts):
+
+// ```js
+// const facts = [
+//   "He was the last Beatle to learn to drive",
+//   "He was never a vegetarian",
+//   "He was a choir boy and boy scout",
+//   "He hated the sound of his own voice"
+// ];
+// ```
+
+// Use a while loop to loop over the facts array and add `"!!!"` to the end of
+// every fact. The function should return an array of strings with exclamation
+// points.
+
+function johnLennonFacts(facts) {
+var resultArray = []; 
+
+var i = 0;
+
+while (i < facts.length) {
+  var str = facts + '!!!';
+}
+  i++;
+  
+ return resultArray.push(str);
+}
